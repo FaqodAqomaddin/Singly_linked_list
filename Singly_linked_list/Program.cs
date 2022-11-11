2692ff68059ea27032ec44244182146740ed996d
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-11 17:11:42 +0700

    Add project files.

---
+++ Singly_linked_list/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singly_linked_list
+{
+    //each node consist of the information part and lik to the next mode
+
+    class Node
+    {
+        public int rolNumber;
+        public string name;
+        public Node next;
+    }
+}
